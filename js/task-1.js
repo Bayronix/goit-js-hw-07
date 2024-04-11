e49870299f9d50be0876fd067f39fab9223200bc
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-04-11 09:26:44 +0200

    Com - 1

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll(".item");
+
+console.log(`Total number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const categoriesListh2 = item.querySelector("h2").textContent;
+  const categorSumOfLiItems = item.querySelectorAll("li > ul");
+  console.log(`Categories: ${categoriesListh2}`);
+  console.log(`Elements: ${categorSumOfLiItems}`);
+});
